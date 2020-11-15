89b722adbbaffeccdaa15a4e2c66231d07144db3
zhangfan <cyouho@hotmail.co.jp>
2020-11-15 22:18:54 +0900

    main_add many file

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,13 @@
-$(function() {
+// 全局header和footer导入用js
+$(function () {
     $("#global_header").load("../global_header.html")
     $("#global_footer").load("../global_footer.html")
+})
+
+
+$(document).ready(function () {
+    // 假登录按钮的弹框用js
+    $("#login").click(function () {
+        alert('都说没有登录功能了，干嘛还点【登录】呢ww')
+    })
 })
