ab5d21c9315d200a57d22010d0c3f56199a1dbba
zhangfan <cyouho@hotmail.co.jp>
2020-11-16 16:51:07 +0900

    main_change login js file contents

--- public/js/index.js
+++ public/js/index.js
@@ -3,11 +3,3 @@ $(function () {
     $("#global_header").load("../global_header.html")
     $("#global_footer").load("../global_footer.html")
 })
-
-
-$(document).ready(function () {
-    // 假登录按钮的弹框用js
-    $("#login").click(function () {
-        alert('都说没有登录功能了，干嘛还点【登录】呢ww')
-    })
-})
--- public/js/login.js
+++ public/js/login.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
     $("#login").click(function () {
-        alert("test")
+        alert("登录功能没完成(就是摆看的ww)")
     })
 })
