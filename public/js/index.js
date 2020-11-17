90f7b1aeb1a07209dffda4509f3c6d8ee0484694
zhangfan <cyouho@hotmail.co.jp>
2020-11-17 20:52:04 +0900

    main_add goto top button

--- public/js/index.js
+++ public/js/index.js
@@ -3,3 +3,8 @@ $(function () {
     $("#global_header").load("../global_header.html")
     $("#global_footer").load("../global_footer.html")
 })
+
+// 回到顶部按钮
+$(function () {
+    $("#gotoTop").load("../goto_top.html")
+})
