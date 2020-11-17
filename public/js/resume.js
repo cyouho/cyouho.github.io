8c3edba6e24ae59660b1912b372cb1884e988ad8
zhangfan <cyouho@hotmail.co.jp>
2020-11-17 19:27:04 +0900

    main_test

---
+++ public/js/resume.js
@@ -0,0 +1,10 @@
+$(function () {
+    $("#bulletin1").load("../my_resume/my_career.html")
+    $("#rule1").load("../my_resume/my_school.html")
+})
+
+$(function () {
+    $('#myTab a').click(function () {
+        $(this).tab('show')
+    })
+})
