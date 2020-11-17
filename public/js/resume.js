3b08377c59f1b463bc306e2fbe6f9d1887848250
zhangfan <cyouho@hotmail.co.jp>
2020-11-18 01:27:58 +0900

    main_add something

--- public/js/resume.js
+++ public/js/resume.js
@@ -2,6 +2,7 @@ $(function () {
     $("#career").load("../my_resume/my_career.html")
     $("#skill").load("../my_resume/my_skill.html")
     $("#school").load("../my_resume/my_school.html")
+    $("#careerDetails").load("../my_resume/my_career_details.html")
 })
 
 $(function () {
