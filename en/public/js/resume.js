dd2c68ef53a3e0442e33c5335f60914f5f23f039
cyouho <cyouho@hotmail.co.jp>
2020-11-25 23:14:31 +0900

    main_add en contents

--- en/public/js/resume.js
+++ en/public/js/resume.js
@@ -1,8 +1,8 @@
 $(function () {
-    $("#career").load("../en//my_resume/my_career.html")
-    $("#skill").load("../en//my_resume/my_skill.html")
-    $("#school").load("../en/my_resume/my_school.html")
-    $("#careerDetails").load("../en//my_resume/my_career_details.html")
+    $("#career").load("../en/my_career.html")
+    $("#skill").load("../en/my_skill.html")
+    $("#school").load("../en/my_school.html")
+    $("#careerDetails").load("../en/my_career_details.html")
 })
 
 $(function () {
