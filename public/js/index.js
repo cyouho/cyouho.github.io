b29c7b56587be54d3fb7837288f2221df12ed3fd
zhangfan <cyouho@hotmail.co.jp>
2021-09-21 14:19:23 +0800

    add sticker

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,9 @@
-// 全局header和footer导入用js
+// 全局header，全局footer，首页广告轮显 导入用js
 $(function () {
     $("#global_header").load("../global_header.html")
     $("#global_footer").load("../global_footer.html")
+    $("#index_slide").load("../index_slide.html")
+    $("#index_my_sticker").load("../my_sticker/my_sticker.html")
 })
 
 // 回到顶部按钮
